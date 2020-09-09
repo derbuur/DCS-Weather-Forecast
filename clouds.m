c62fe3438e071b47e42fec3fc76ae6f33a6e87ea
Thomas Baur <derbuur@googlemail.com>
2020-09-09 16:51:04 +0200

    rewrite clouds
    rewrite the clouds function. Now clouds depend on the absolut pressure
    and the gradient

--- clouds.m
+++ clouds.m
@@ -1,64 +1,47 @@
 function [PgesR] = clouds(pressure_spread,pressure_excess,centerZ,centerX,xdim_unten,xdim_oben,zdim_links,zdim_rechts,ellipticity,rotation,WeatherName)
-% This function generates the map with the clouds
-
-
-figure('Name',WeatherName)
+figure('Name',WeatherName,'visible','off')
 t = linspace(0,2*pi,100)'; 
 
 gridsize = 100000;
-%--------------- Hintergrundbild einfügen
 
-  img = imread('CaucasusSatellite.png');
+##  cmap = [1 1 1 %white
+##          0 0 0]; % black
+ [PgesR,Z,X] = IsobarenCalculation(gridsize,-2000000,  2000000, -2000000, 2000000,pressure_spread,pressure_excess,centerX,centerZ,ellipticity,rotation);
+       
  
+  [WZ,WX] = gradient(PgesR);
+dabf= sqrt(WZ.^2 + WX.^2);
+relC = PgesR < 101325;
+%colormap('gray');
+[h]=pcolor(Z,X,dabf.*relC);
 
-  min_x = -2000000;
-  max_x = 2000000;
-  min_y = -2000000;
-  max_y = 2000000;
-  
-  
-  imagesc([min_y max_y],[  max_x  min_x], img);
+shading interp;
+ axis tight;
 
-  hold on
-  set(gca,'ydir','normal');
-
-  %--------------- Hintergrundbild einfügen ende
-  cmap = [1 1 1 %white
-          0 0 0]; % black
-        
-  hold on
-
-
-[PgesR,Z,X] = IsobarenCalculation(gridsize,-2000000,  2000000, -2000000, 2000000,pressure_spread,pressure_excess,centerX,centerZ,ellipticity,rotation);
-set(gca,'ydir','normal');
-
-
-PgesR(PgesR>101325)=NaN;
-
-P_scalde=( max(max(PgesR)) - PgesR ) ./ ( max(max(PgesR)) - min(min(PgesR)) );
-
-
-Xrand = X+(rand(size(X))-0.5)*gridsize;
-Zrand = Z+(rand(size(Z))-0.5)*gridsize;
-
-  scatter (Zrand, Xrand, (P_scalde+0.0001)*350,"w","filled")
 
 set(gca,'DataAspectRatio',[1000000 1000000 1])
 colormap('gray');
 
-
-
+%set(h,'EdgeAlpha',0,'FaceAlpha',0.7);
+%set(h,'facealpha',0.3)
 drawnow ()
 
-hold off
 
-caxis([913 1113])
+%caxis([913 1113])
 
-set(gca,'xtick',[])
-  set(gca,'xticklabel',[])
-  set(gca,'ytick',[])
-  set(gca,'yticklabel',[])
+  set(gca,'xtick',[]);
+  set(gca,'xticklabel',[]);
+  set(gca,'ytick',[]);
+  set(gca,'yticklabel',[]);
   
 
-%print ('clouds.png', "-S819,819")
+print ('clouds.png', "-S819,819")
+system("magick convert clouds.png -crop 635x634+106+79 -resize 819x819 cloudscrop.png");
+
+system("magick convert cloudscrop.png -background black -alpha copy -type truecolormatte PNG32:cloudscropT.png");
+
+system ("magick composite -compose atop cloudscropT.png CaucasusSatellite.png CaucasusOverClouds.png");
+system ("magick composite -compose atop CaucasusBorders.png CaucasusOverClouds.png CaucasusOverCloudsBoarders.png");
+system("CaucasusOverCloudsBoarders.png");
 endfunction
+
